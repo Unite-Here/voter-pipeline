8fbd7cc21919897b4f3998ee0f1756c038553d27
Nola Watson <nwatson@unitehere.org>
2024-10-04 15:52:13 -0400

    The validate_input function returns true for input of None

--- test/test_utils/test_data.py
+++ test/test_utils/test_data.py
@@ -1,7 +1,19 @@
 from utils.data import validate_input
 
 
-# Test validate_input returns valid
+# Test validate_input returns true if input is None
+def test_validate_input_none():
+    # Fake variables
+    fake_input = None
+    fake_valid_list = ["Orange", "Lime", "Lemon", "Grapefruit"]
+
+    # Call validate_input
+    response = validate_input(fake_valid_list, fake_input)
+
+    # Assert input is valid
+    assert response == True
+
+
 def test_validate_input_valid():
     # Fake variables
     fake_input = "Lime"
--- utils/data.py
+++ utils/data.py
@@ -12,5 +12,9 @@ def validate_input(valid_input: list, in_val) -> bool:
     bool
         If in_val matches an item in valid_input then returns True, else False
     """
-    valid = any(in_val in valid_input for item in valid_input)
-    return valid
+
+    if in_val == None:
+        return True
+    else:
+        valid = any(in_val in valid_input for item in valid_input)
+        return valid
