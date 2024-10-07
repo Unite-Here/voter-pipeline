6a50d87e08fda1eef4241c38125fff930f13959a
Nola Watson <nwatson@unitehere.org>
2024-10-07 14:13:44 -0400

    All tests passing

--- test/test_scraper/test_base_vote_scraper.py
+++ test/test_scraper/test_base_vote_scraper.py
@@ -16,7 +16,7 @@ def create_mock_future(client, response):
     return CivisFuture(mock_poller, mock_poller_args, None)
 
 
-# TODO: test db_upsert success
+# Test db_upsert success
 def test_db_upsert_success(mocker):
     # Fake variables
     fake_scraper = BaseVoterScraper()
@@ -96,3 +96,76 @@ def test_db_upsert_success(mocker):
 
 
 # TODO: test db_upsert fail
+def test_db_upsert_fail(mocker):
+    fake_scraper = BaseVoterScraper()
+    fake_votes = [{
+        "IDNUMBER": "123456",
+        "COUNTY": "CLARK_COUNTY",
+        "VOTE_TYPE": "Mail",
+        "ELECTION_CODE": "0000",
+        "ACTIVITY_DATE": "2000-01-01",
+        "NAME": "Un Known",
+        "PRECINCT": "0",
+        "PARTY": "0",
+        "PARTY_NAME": "Null Party",
+        "CONGRESS": "0",
+        "ASSEMBLY": "0",
+        "SENATE": "0",
+        "COMMISSION": "0",
+        "EDUCATION": "0",
+        "REGENT": "0",
+        "SCHOOL": "0",
+        "CITY": "0",
+        "WARD": "0",
+        "TOWNSHIP": "0",
+        "REG_STATUS": "A",
+        "VOTE_SITE": "0"
+    }, {
+        "IDNUMBER": "654321",
+        "COUNTY": "CLARK_COUNTY",
+        "VOTE_TYPE": "Early",
+        "ELECTION_CODE": "0000",
+        "ACTIVITY_DATE": "2000-01-01",
+        "NAME": "No One",
+        "PRECINCT": "0",
+        "PARTY": "0",
+        "PARTY_NAME": "Null Party",
+        "CONGRESS": "0",
+        "ASSEMBLY": "0",
+        "SENATE": "0",
+        "COMMISSION": "0",
+        "EDUCATION": "0",
+        "REGENT": "0",
+        "SCHOOL": "0",
+        "CITY": "0",
+        "WARD": "0",
+        "TOWNSHIP": "0",
+        "REG_STATUS": "A",
+        "VOTE_SITE": "0"
+    }]
+    
+    fake_voters = [
+        Voter(fake_votes[0]["IDNUMBER"], fake_votes[0]["COUNTY"], fake_votes[0]["VOTE_TYPE"],
+              fake_votes[0]["ELECTION_CODE"], fake_votes[0]["ACTIVITY_DATE"], fake_votes[0]["NAME"],
+              fake_votes[0]["PRECINCT"], fake_votes[0]["PARTY"], fake_votes[0]["PARTY_NAME"], fake_votes[0]["CONGRESS"],
+              fake_votes[0]["ASSEMBLY"], fake_votes[0]["SENATE"], fake_votes[0]["COMMISSION"],
+              fake_votes[0]["EDUCATION"], fake_votes[0]["REGENT"], fake_votes[0]["SCHOOL"], fake_votes[0]["CITY"],
+              fake_votes[0]["WARD"], fake_votes[0]["TOWNSHIP"], fake_votes[0]["REG_STATUS"],
+              fake_votes[0]["VOTE_SITE"]),
+        Voter(fake_votes[1]["IDNUMBER"], fake_votes[1]["COUNTY"], fake_votes[1]["VOTE_TYPE"],
+              fake_votes[1]["ELECTION_CODE"], fake_votes[1]["ACTIVITY_DATE"], fake_votes[1]["NAME"],
+              fake_votes[1]["PRECINCT"], fake_votes[1]["PARTY"], fake_votes[1]["PARTY_NAME"], fake_votes[1]["CONGRESS"],
+              fake_votes[1]["ASSEMBLY"], fake_votes[1]["SENATE"], fake_votes[1]["COMMISSION"],
+              fake_votes[1]["EDUCATION"], fake_votes[1]["REGENT"], fake_votes[1]["SCHOOL"], fake_votes[1]["CITY"],
+              fake_votes[1]["WARD"], fake_votes[1]["TOWNSHIP"], fake_votes[1]["REG_STATUS"], fake_votes[1]["VOTE_SITE"])
+    ]
+
+    # Mock function call
+    mock_csv_to_civis = mocker.patch("lib.scraper.base_vote_scraper.civis.io.csv_to_civis", side_effect=Exception)
+
+    # Set fake_scraper clean_votes list using fake_voters
+    fake_scraper.clean_votes = fake_voters
+
+    # Assert exception raised
+    with pytest.raises(BaseScraperError, match=r"Error upserting data to civis"):
+        fake_scraper.db_upsert()
